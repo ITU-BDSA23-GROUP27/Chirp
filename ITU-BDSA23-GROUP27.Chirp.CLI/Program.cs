edf68375bd624f244ad33b3268c3156e914150e8
phiy@itu.dk <phiy@itu.dk>
2023-09-05 13:31:33 +0200

    Added function for Main, Read, Cheep, and time/date converter
    Co-authored-by: Tien, Patrick, Kasper, Omar

--- ITU-BDSA23-GROUP27.Chirp.CLI/Program.cs
+++ ITU-BDSA23-GROUP27.Chirp.CLI/Program.cs
@@ -1,18 +1,103 @@
-﻿if (args.Length == 0)
+﻿using System.Globalization;
+
+string FILE = "chirp_cli_db.csv";
+string DATE_FORMAT = "MM/dd/yy HH:mm:ss";
+
+void Main()
 {
-    Console.WriteLine("Missing argument \n  " +
-                      "-read : to read all messages \n  " +
-                      "-cheep 'Your Message' : post a message");
-    return;
+    if (args.Length < 1)
+    {
+        Console.WriteLine("Usage: \n  " +
+                        " - dotnet <read> \n  " +
+                        " - dotnet <cheep> [message]");
+        return;
+    }
+
+    if (args[0] == "read")
+    {
+        ReadChirps();
+    }
+    else if (args[0] == "cheep")
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("A cheep message is missing!");
+            return;
+        }
+
+        Cheep(args[1]);
+    }
+    else
+    {
+        Console.WriteLine("Invalid command. Use 'read' or 'cheep'.");
+    }
 }
 
-switch (args[0])
+void ReadChirps()
 {
-    case "-read":
-        break;
-    case "-cheep":
-        break;
-    default:
-        Console.WriteLine("Invalid Argument");
-        return;
+    try
+    {
+        string[] lines = File.ReadAllLines(FILE);
+
+        for (int i = 1; i < lines.Length; i++) // skip header line
+        {
+            string[] data = lines[i].Split('"');
+
+            string author = data[0].TrimEnd(',');
+            string message = data[1];
+            string timestamp = data[2].TrimStart(',');
+
+            string datetime = TimestampToDate(timestamp);
+
+            string chirp = $"{author} @ {datetime}: {message}";
+            Console.WriteLine(chirp);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
 }
+
+void Cheep(string message)
+{
+    try
+    {
+        string username = Environment.UserName;
+        long timestamp = DateToTimestamp(DateTime.Now.ToString(DATE_FORMAT, CultureInfo.InvariantCulture));
+
+        string chirp = $"{username},\"{message}\",{timestamp}";
+        File.AppendAllText(FILE, chirp + Environment.NewLine);
+
+        Console.WriteLine("Text appended successfully.");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("An error occurred while cheeping.");
+    }
+}
+
+string TimestampToDate(string timestamp)
+{
+    if (int.TryParse(timestamp, out int unixTimestamp))
+    {
+        DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTimestamp);
+        string formattedDateTime = dateTimeOffset.ToLocalTime().ToString(DATE_FORMAT, CultureInfo.InvariantCulture);
+        return formattedDateTime;
+    }
+
+    return "Invalid Timestamp";
+}
+
+long DateToTimestamp(string datetime)
+{
+    if (DateTime.TryParseExact(datetime, DATE_FORMAT, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime datetimeParse))
+    {
+        long timestamp = new DateTimeOffset(datetimeParse).ToUnixTimeSeconds();
+        return timestamp;
+    }
+
+    return 0;
+}
+
+Main();
