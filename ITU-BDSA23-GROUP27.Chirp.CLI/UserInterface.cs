b3ec61f73c8eefa15b2d2728c78ff116e4b7ddd8
Patrick Søborg <ptso@itu.dk>
2023-09-12 14:49:03 +0200

    UserInterface.cs updated PrintCheeps function with limit that allows for limiting the amount of cheeps displayed in the console on the command call --r <limit> || --read <limit>
    Co-authored-by: Kasper Kirkegaard Nielsen <kkni@itu.dk>
    Co-authored-by: Phi Va Lo <phiy@itu.dk>
    Co-authored-by: Omar Lukman Semou <omse@itu.dk>
    Co-authored-by: Tien Cam Ly <tily@itu.dk>

--- ITU-BDSA23-GROUP27.Chirp.CLI/UserInterface.cs
+++ ITU-BDSA23-GROUP27.Chirp.CLI/UserInterface.cs
@@ -5,12 +5,27 @@ public class UserInterface
 {
     const string DATE_FORMAT = "MM/dd/yy HH:mm:ss";
     
-    public static void PrintCheeps(IEnumerable<Cheep> Cheeps)
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps, long? limit)
     {
-        foreach (var (author, message, timestamp) in Cheeps)
+        var list = cheeps.ToList();
+        
+        switch (limit)
         {
+            case null:
+                limit = list.Count;
+                break;
+            
+            case <= 0:
+                throw new ArgumentException("Limit cannot be less than or equal to zero");
+        }
+        
+        int i = 0;
+        foreach (var (author, message, timestamp) in list)
+        {
+            if (i == limit) break;
             string chirp = $"{author} @ {TimestampToDate(timestamp.ToString())}: {message}";
             Console.WriteLine(chirp);
+            i++;
         }
     }
     
