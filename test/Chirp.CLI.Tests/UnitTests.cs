98c1f415f46b682e013a3640b8ccf5ff3d9aa272
Phi Va Lo <phiy@itu.dk>
2023-09-19 16:49:44 +0200

    Added more unit tests and reformatted for multiple tests

---
+++ test/Chirp.CLI.Tests/UnitTests.cs
@@ -0,0 +1,30 @@
+namespace Chirp.CLI.Tests;
+using ITU_BDSA23_GROUP27.Chirp.CLI;
+
+public class UnitTests
+{
+
+    [Theory]
+    [InlineData("08/01/23 14:09:20", 1690891760)]
+    [InlineData("08/02/23 14:19:38", 1690978778)]
+    [InlineData("08/02/23 14:37:38", 1690979858)]
+    [InlineData("08/02/23 15:04:47", 1690981487)]
+    public void DateToTimestamp_Convert_ReturnsTimestamp(string input, long expected)
+    {
+        long actual = Utility.DateToTimestamp(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("1690891760", "08/01/23 14:09:20")]
+    [InlineData("1690978778", "08/02/23 14:19:38")]
+    [InlineData("1690979858", "08/02/23 14:37:38")]
+    [InlineData("1690981487", "08/02/23 15:04:47")]
+    public void TimestampToDate_Convert_ReturnsDate(string input, string expected)
+    {
+        string actual = Utility.TimestampToDate(input);
+
+        Assert.Equal(expected, actual);
+    }
+}
