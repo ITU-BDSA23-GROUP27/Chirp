5ccc0fc634981360f188d52385e9dd7d448c1a2a
Tien Cam Ly <tily@itu.dk>
2023-09-20 14:19:30 +0200

    Added triple A's comments to the tests for better overview

--- test/SimpleDB.Tests/IntegrationTests.cs
+++ test/SimpleDB.Tests/IntegrationTests.cs
@@ -6,15 +6,17 @@ public class IntegrationTests
     [Fact]
     public void CSVDatabase_Cheep_Read_ReturnsLastCheep()
     {
+        // Arrange
         string filepath = "../../../../../data/chirp_cli_db.csv";
         IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(filepath);
-        
         Cheep entry = new Cheep(Environment.UserName, "message", 1690891760);
+        
+        // Act
         database.Store(entry);
-
         var cheeps = database.Read().ToArray();
         var lastCheep = cheeps[^1];
     
+        // Assert
         Assert.Equal("message", lastCheep.Message);
     }
     
