2b3fe43610823edcd5a4e34670bface85b5888a8
Phi Va Lo <phiy@itu.dk>
2023-09-21 18:17:22 +0200

    Updated E2E - semi working

--- test/Chirp.CLI.Tests/End-To-EndTests.cs
+++ test/Chirp.CLI.Tests/End-To-EndTests.cs
@@ -23,12 +23,12 @@ public class End_To_EndTests
 
         // Act
         string output = "";
-        string commandLineArguments = "--read";
+        // string commandLineArguments = "read 10";
         
         using (var process = new Process())
         {
             process.StartInfo.FileName = "C:/Program Files/dotnet/dotnet.exe";
-            process.StartInfo.Arguments = $"./src/ITU-BDSA23-GROUP27.Chirp.CLI/bin/Debug/net7.0/ITU-BDSA23-GROUP27.Chirp.CLI.dll {commandLineArguments}";
+            process.StartInfo.Arguments = $"./src/ITU-BDSA23-GROUP27.Chirp.CLI/bin/Debug/net7.0/ITU-BDSA23-GROUP27.Chirp.CLI.dll read";
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.WorkingDirectory = "../../../../../";
             process.StartInfo.RedirectStandardOutput = true;
@@ -42,20 +42,13 @@ public class End_To_EndTests
 
         string fstCheep = output.Split("\n")[0];
 
+        fstCheep = fstCheep.TrimEnd('\r', '\n');
+ 
         _testOutputHelper.WriteLine(fstCheep);
-        
+
         // Assert
         Assert.StartsWith("ropf", fstCheep);
-        Assert.EndsWith("Hello, World!", fstCheep);
-    }
-
-    // checks if Chirp returns the same amount of Cheeps as limit
-    public void Commandline_Read_ReturnsLimit()
-    {
-        // Arrange
-        
-        // Act
-        
-        // Assert
+        Assert.EndsWith("Hello, BDSA students!", fstCheep);
     }
 }
+
