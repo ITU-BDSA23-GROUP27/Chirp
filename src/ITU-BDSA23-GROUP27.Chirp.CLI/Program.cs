1a2304eec8ae1cd0b53932ba522f7d6529b67a6c
PTS404 <patricksoborg@protonmail.com>
2023-09-21 17:03:20 +0200

    Refactored Chirp.CLI to use the CSV Database Web Service
    /cheep + /cheeps works on localhost between client and server

--- src/ITU-BDSA23-GROUP27.Chirp.CLI/Program.cs
+++ src/ITU-BDSA23-GROUP27.Chirp.CLI/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System.Globalization;
 using CommandLine;
 using ITU_BDSA23_GROUP27.Chirp.CLI;
-using SimpleDB;
+using System.Text;
+using Newtonsoft.Json;
 
-var types = CommandLineOptions.GetVerbs();            
+var types = CommandLineOptions.GetVerbs();
+
+// HTTP Client Creation
+var baseURL = "http://localhost:5100";
+using HttpClient client = new();
+client.BaseAddress = new Uri(baseURL);
 
 Parser.Default.ParseArguments(args, types)
     .WithParsed(Run)
@@ -14,31 +20,37 @@ static void HandleErrors(object obj)
     // TODO Implement error handling
 }
         
-static void Run(object obj)
+void Run(object obj)
 {
     switch (obj)
     {
         case CommandLineOptions.CheepOptions c when c.Message != null:
-            Cheep(c.Message);
+            Cheep(c.Message).Wait();
             break;
         case CommandLineOptions.ReadOptions r:
-            ReadCheeps(r.Limit);
+            ReadCheeps(r.Limit).Wait();
             break;
         case CommandLineOptions.Options a:
             break;
     }
 }
 
-static void Cheep(string message)
+async Task Cheep(string message)
 {
     long timestamp = Utility.DateToTimestamp(DateTime.Now.ToString(Utility.DATE_FORMAT, CultureInfo.InvariantCulture));
     Cheep record = new Cheep(Environment.UserName, message, timestamp);
-    CSVDatabase<Cheep>.Instance.Store(record);
+    var http = new StringContent(JsonConvert.SerializeObject(record), Encoding.UTF8, "application/json");
+    await client.PostAsync("/cheep", http);
 }
 
-static void ReadCheeps(int? limit)
+async Task ReadCheeps(int? limit)
 {
-    UserInterface.PrintCheeps(CSVDatabase<Cheep>.Instance.Read(), limit);
+    var response = await client.GetAsync("/cheeps");
+    
+    var responseContent = await response.Content.ReadAsStringAsync();
+    IEnumerable<Cheep> ?cheeps = JsonConvert.DeserializeObject<List<Cheep>>(responseContent);
+    
+    UserInterface.PrintCheeps(cheeps, limit);
 }
 
 public record Cheep(string Author, string Message, long Timestamp);
--- src/ITU-BDSA23-GROUP27.Chirp.CLI/UserInterface.cs
+++ src/ITU-BDSA23-GROUP27.Chirp.CLI/UserInterface.cs
@@ -3,15 +3,22 @@ namespace ITU_BDSA23_GROUP27.Chirp.CLI;
 
 public static class UserInterface
 {
-    public static void PrintCheeps(IEnumerable<Cheep> cheeps, int? limit)
+    public static void PrintCheeps(IEnumerable<Cheep> ?cheeps, int? limit)
     {
+        if (cheeps is null) { return; }
+        
         var list = cheeps.ToList();
         int readLimit = limit ?? list.Count;
 
-        if (readLimit <= 0)
+        //Error handling
+        if (list.Count == 0)
         {
-            throw new ArgumentException("limit must be null or greater than 0");
+            throw new ArgumentException("There are no cheeps stored");
         }
+        if (readLimit < 0)
+        {
+            throw new ArgumentException("Limit must be a positive integer");
+        } 
         
         int counter = 0;
         
