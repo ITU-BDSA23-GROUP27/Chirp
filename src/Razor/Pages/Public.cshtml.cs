9bc8a6803b0133d342240f093f22496f5dffd524
PTS404 <ptso@itu.dk>
2023-10-04 13:55:11 +0200

    Added cheep pagination
    Co-authored-by: Tien Cam Ly <tily@itu.dk>
    Co-authored-by: Phi Va Lo <phiy@itu.dk>

--- src/Razor/Pages/Public.cshtml.cs
+++ src/Razor/Pages/Public.cshtml.cs
@@ -8,6 +8,8 @@ public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
+    public int CurrentPage { get; set; } = 1;
+    public int MaxCheepsPerPage { get; } = 32;
 
     public PublicModel(ICheepService service)
     {
@@ -16,6 +18,12 @@ public class PublicModel : PageModel
 
     public ActionResult OnGet()
     {
+        //The following if statement has been made with the help of CHAT-GPT
+        if (int.TryParse(Request.Query["page"], out int parsedPage) && parsedPage > 0)
+        {
+            CurrentPage = parsedPage;
+        }
+        
         Cheeps = _service.GetCheeps();
         return Page();
     }
