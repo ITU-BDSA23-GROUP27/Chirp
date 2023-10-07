e847e7255516e89d932f4ce6a944aa3ac402ad6f
Tien Cam Ly <tily@itu.dk>
2023-10-07 19:12:14 +0200

    Implemented EFCore
    Co-authored-by: Patrick Søborg <ptso@itu.dk>
    
    Co-authored-by: Phi Va Lo <phiy@itu.dk>

--- src/CheepRepository/Model.cs
+++ src/CheepRepository/Model.cs
@@ -1,19 +1,47 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace CheepRepository;
 
-public class Model
+public class CheepContext : DbContext
 {
+    public DbSet<Cheep> Cheeps { get; set; }
+    public DbSet<Author> Authors { get; set; } 
+    
+    public string DbPath { get; }
+    public CheepContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "chirp.db");
+    }
+    
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+    
 }
 
 public class Cheep
 {
-    public string Text { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public required string Text { get; set; }
     public DateTime TimeStamp { get; set; }
-    public Author Author { get; set; }
+    public required Author Author { get; set; }
 }
 
 public class Author
 {
-    public string Name { get; set; }
-    public string Email { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public required string Name { get; set; }
+    public required string Email { get; set; }
     public List<Cheep> Cheeps { get; set; } = new();
 }
