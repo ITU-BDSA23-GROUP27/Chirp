f7808210459ed4ecd4f4ad85f5334832095abdff
Tien Cam Ly <tily@itu.dk>
2023-10-19 21:14:22 +0200

    Updated packages and renamed namespace in UserTimeline
    Co-authored-by: Phi Va Lo <phiy@itu.dk>
    Co-authored-by: Patrick Søborg <ptso@itu.dk>
    Co-authored-by: Omar Lukman Semou <omse@itu.dk>

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -3,7 +3,7 @@ using Chirp.Core.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Chirp.Razor.Pages;
+namespace Chirp.Web.Pages;
 
 public class UserTimelineModel : PageModel
 {
