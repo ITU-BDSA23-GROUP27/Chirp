39c76c2083a6fd9da70b39948b7fc74e9abbac20
Omar Lukman Semou <omse@itu.dk>
2023-10-24 00:21:26 +0200

    Added Xunit tests for various methods for CheepRepository and AuthorRepository under Chirp.Infrastructure
    Author: Omar Lukman Semou <omse@itu.dk>

---
+++ test/Chirp.Infrastructure.Test/UnitTest.cs
@@ -0,0 +1,136 @@
+using Chirp.Core.DTOs;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirp.Infrastructure.Test
+{
+    public class RepositoryTests : IDisposable
+    {
+        private readonly ChirpContext _context;
+        private readonly Infrastructure.CheepRepository _cheepRepository;
+        private readonly AuthorRepository _authorRepository;
+
+        public RepositoryTests()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<ChirpContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            _context = new ChirpContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            _cheepRepository = new Infrastructure.CheepRepository(_context);
+            _authorRepository = new AuthorRepository(_context);
+        }
+
+        [Fact]
+        public void CreateAuthor_Success()
+        {
+            // Arrange
+            var authorDto = new AuthorDto { Name = "Bodil Bodilsen", Email = "Bodil@danmark.dk" };
+
+            // Act
+            _authorRepository.CreateAuthor(authorDto);
+
+            // Assert
+            var author = _context.Authors.Single(a => a.Name == "Bodil Bodilsen");
+            Assert.Equal("Bodil Bodilsen", author.Name);
+            Assert.Equal("Bodil@danmark.dk", author.Email);
+        }
+
+        [Fact]
+        public void CreateCheep_Success()
+        {
+            // Arrange
+            var authorDto = new AuthorDto { Name = "Hans Hansen", Email = "HH@outlook.com" };
+            _authorRepository.CreateAuthor(authorDto);
+            var cheepDto = new CheepDto
+            {
+                Message = "Bye, world!",
+                TimeStamp = DateTime.UtcNow.ToString(),
+                AuthorName = "Hans Hansen"
+            };
+
+            // Act
+            _cheepRepository.CreateCheep(cheepDto);
+
+            // Assert
+            var cheep = _context.Cheeps.Single();
+            Assert.Equal("Bye, world!", cheep.Text);
+            Assert.Equal("Hans Hansen", cheep.Author.Name);
+        }
+
+        [Fact]
+        public void GetCheeps_ReturnsAllCheeps()
+        {
+            // Arrange
+            var author = new AuthorDto { Name = "Omar Semou", Email = "OmarSemou@hotmail.com" };
+            _authorRepository.CreateAuthor(author);
+            var cheep = new CheepDto { AuthorName = "Omar Semou", Message = "Testing 1 2 3", TimeStamp = DateTime.UtcNow.ToString() };
+            _cheepRepository.CreateCheep(cheep);
+
+            // Act
+            var result = _cheepRepository.GetCheeps();
+
+            // Assert
+            Assert.Single(result);
+        }
+
+        [Fact]
+        public void GetCheepsFromPage_ReturnsCorrectPage()
+        {
+            // Arrange
+            var author = new AuthorDto { Name = "Darryl Davidson", Email = "merica4lyfe@usa.com" };
+            _authorRepository.CreateAuthor(author);
+            for (int i = 0; i < 50; i++)
+            {
+                var cheep = new CheepDto { AuthorName = "Darryl Davidson", Message = $"I ain't afriad {i}", TimeStamp = DateTime.UtcNow.ToString() };
+                _cheepRepository.CreateCheep(cheep);
+            }
+
+            // Act
+            var result = _cheepRepository.GetCheepsFromPage(2);
+
+            // Assert
+            Assert.Equal(18, result.Count());  // Assuming page size of 32
+        }
+
+        [Fact]
+        public void GetAuthorByName_ReturnsCorrectAuthor()
+        {
+            // Arrange
+            var author = new AuthorDto { Name = "Spongebob Squarepants", Email = "mrgoofy@pants.com" };
+            _authorRepository.CreateAuthor(author);
+
+            // Act
+            var result = _authorRepository.GetAuthorByName("Spongebob Squarepants");
+
+            // Assert
+            Assert.Equal("Spongebob Squarepants", result.Name);
+        }
+
+        [Fact]
+        public void GetAuthorByEmail_ReturnsCorrectAuthor()
+        {
+            // Arrange
+            var author = new AuthorDto { Name = "Karsten Pedersen", Email = "kp67@email.com" };
+            _authorRepository.CreateAuthor(author);
+
+            // Act
+            var result = _authorRepository.GetAuthorByEmail("kp67@email.com");
+
+            // Assert
+            Assert.Equal("Karsten Pedersen", result.Name);
+        }
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+    }
+}
