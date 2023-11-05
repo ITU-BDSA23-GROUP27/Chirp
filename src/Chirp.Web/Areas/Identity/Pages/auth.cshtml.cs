2c18b9a6c4ac2cacdc27031652edf82c62e13072
Kasper Kirkegaard <kkni@itu.dk>
2023-11-06 00:19:43 +0100

    Added auth cs.html page -ignore for now

---
+++ src/Chirp.Web/Areas/Identity/Pages/auth.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Areas.Identity.Pages;
+
+[Authorize]
+public class auth : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
