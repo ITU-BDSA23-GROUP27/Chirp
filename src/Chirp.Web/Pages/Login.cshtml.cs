70b48a4cfc699c2a2f24129abf32d3666b655422
Kasper Kirkegaard <kkni@itu.dk>
2023-11-08 01:07:46 +0100

    Moved Login feature out of _LoginPartial.cshtml and into Login page (May be subject to change) Added PostRequest to authenticate via. github

---
+++ src/Chirp.Web/Pages/Login.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages;
+
+[AllowAnonymous]
+public class LoginModel : PageModel
+{
+    public IActionResult OnPostAuthenticateLogin()
+    {
+        var props = new AuthenticationProperties
+        {
+            RedirectUri = Url.Page("/Error"),
+        };
+        return Challenge(props);
+    }
+}
---
+++ src/Chirp.Web/Pages/Shared/_LoginPartial.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages.Shared;
+
+public class LoginPartialModel : PageModel
+{
+    public IResult OnGetAuthenticateLogin()
+    {
+        return Results.Challenge(new AuthenticationProperties(){RedirectUri = "http://localhost:5273/login"}, new List<string>(){"github"});
+    }
+}
