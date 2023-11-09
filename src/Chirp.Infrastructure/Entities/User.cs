34176f31ee38da17329e527e6da5cc09e92e5711
Kasper Kirkegaard Nielsen <kkni@itu.dk>
2023-11-09 15:47:51 +0100

    Removed GUID from user, to use the IdentityUser id
    Co-authored-by: Tien Cam Ly <tily@itu.dk>
    Co-authored-by: Phi Va Lo <phiy@itu.dk>
    Co-authored-by: Patrick Søborg <ptso@itu.dk>

--- src/Chirp.Infrastructure/Entities/User.cs
+++ src/Chirp.Infrastructure/Entities/User.cs
@@ -6,10 +6,6 @@ namespace Chirp.Infrastructure.Entities;
 
 public class User : IdentityUser
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public Guid UserId { get; set; }
-    
     public required string Name { get; set; }
     public required string Email { get; set; }
     public IEnumerable<Cheep> Cheeps { get; set; } = new List<Cheep>();
