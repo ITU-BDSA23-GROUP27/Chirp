ce93243d1a27abce828597f99a688b0feffc95c6
Kasper Kirkegaard Nielsen <kkni@itu.dk>
2023-11-09 15:50:43 +0100

    Added migrations and updated database to support IdentityUsers
    Co-authored-by: Tien Cam Ly <tily@itu.dk>
    Co-authored-by: Phi Va Lo <phiy@itu.dk>
    Co-authored-by: Patrick Søborg <ptso@itu.dk>

---
+++ src/Chirp.Infrastructure/Migrations/20231109144216_RemovedUserID.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedUserID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cheeps_AspNetUsers_UserId",
+                table: "Cheeps");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Cheeps",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cheeps_AspNetUsers_UserId",
+                table: "Cheeps",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cheeps_AspNetUsers_UserId",
+                table: "Cheeps");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Cheeps",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cheeps_AspNetUsers_UserId",
+                table: "Cheeps",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
