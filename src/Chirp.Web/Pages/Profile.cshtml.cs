4bbcb9ec1ed2401544e1c1e26498824fc9a79016
Phi Va Lo <phiy@itu.dk>
2023-11-10 00:06:31 +0100

    Updated Write-Cheep textbox is now expanded by default. Added new empty-page Profile

---
+++ src/Chirp.Web/Pages/Profile.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages;
+public class ProfileModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
