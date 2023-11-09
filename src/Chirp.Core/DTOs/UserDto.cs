bb560a4cf03ab454886193f93c1bdc978a2d2193
Kasper Kirkegaard Nielsen <kkni@itu.dk>
2023-11-09 15:48:29 +0100

    Changed User id from guid to string to match IdentityUser
    Co-authored-by: Tien Cam Ly <tily@itu.dk>
    Co-authored-by: Phi Va Lo <phiy@itu.dk>
    Co-authored-by: Patrick Søborg <ptso@itu.dk>

--- src/Chirp.Core/DTOs/UserDto.cs
+++ src/Chirp.Core/DTOs/UserDto.cs
@@ -3,7 +3,7 @@ namespace Chirp.Core.DTOs;
 public class UserDto
 {
     // made to public records instead ?
-    public Guid Id { get; set; }
+    public string Id { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
 }
