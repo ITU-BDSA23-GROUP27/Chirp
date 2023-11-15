f8b590e04c9748db1eae63c457a84bee255fddb6
Phi Va Lo <phiy@itu.dk>
2023-11-15 14:51:17 +0100

    Added confirmation alert box to seeding/clearing database

--- src/Chirp.Web/wwwroot/js/seedDB.js
+++ src/Chirp.Web/wwwroot/js/seedDB.js
@@ -1,13 +1,13 @@
 // pop up alert box
 
-function confirmSeed() {
-    if (confirm("Are you sure you want to reset the database?")) {
+function confirmAction(action) {
+    var confirmationMessage = "";
 
+    if (action === 'reset') {
+        confirmationMessage = "Are you sure you want to reset the database?";
+    } else if (action === 'clear') {
+        confirmationMessage = "Are you sure you want to clear the database?";
     }
-}
 
-function confirmClear() {
-    if (confirm("Are you sure you want to clear the database?")) {
-
-    }
+    return confirm(confirmationMessage);
 }
