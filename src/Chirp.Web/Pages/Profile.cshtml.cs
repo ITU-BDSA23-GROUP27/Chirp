c0658d4484768da70e13fab34275041f1b349c01
Phi Va Lo <phiy@itu.dk>
2023-11-17 10:46:12 +0100

    Moved Claims page into Profile page and added delete account button

--- src/Chirp.Web/Pages/Profile.cshtml.cs
+++ src/Chirp.Web/Pages/Profile.cshtml.cs
@@ -3,13 +3,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
+
 public class ProfileModel : PageModel
 {
+    public string? GithubURL { get; set; }
+
     public IActionResult OnGet()
     {
         if (User.Identity?.IsAuthenticated == false)
         {
             return RedirectToPage("/Public");
+        }        
+        else
+        {
+            foreach (var claim in User.Claims)
+            {
+                Console.WriteLine($"Claim Type: {claim.Type}, Claim Value: {claim.Value}");
+            }
+
+            GithubURL = User.FindFirst("urn:github:url")?.Value;
         }
         return Page();
     }
