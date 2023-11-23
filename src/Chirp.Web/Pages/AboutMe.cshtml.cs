f5ead042b4e6edafd6c243cc01130aa0fc4b55f0
Phi Va Lo <phiy@itu.dk>
2023-11-23 15:22:56 +0100

    Updated Claims to to Identity

--- src/Chirp.Web/Pages/AboutMe.cshtml.cs
+++ src/Chirp.Web/Pages/AboutMe.cshtml.cs
@@ -6,7 +6,12 @@ namespace Chirp.Web.Pages;
 
 public class AboutMeModel : BasePageModel
 {
+    public string? ID { get; set; }
+    public string? Username { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
     public string? GithubURL { get; set; }
+    public string? Avatar { get; set; }
 
     public async Task<IActionResult> OnGet()
     {
@@ -17,7 +22,12 @@ public class AboutMeModel : BasePageModel
                 Console.WriteLine($"Claim Type: {claim.Type}, Claim Value: {claim.Value}");
         }
 
+        ID = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+        Username = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
+        Email = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+        Name = User.FindFirst("urn:github:name")?.Value;
         GithubURL = User.FindFirst("urn:github:url")?.Value;
+        Avatar = $"https://avatars.githubusercontent.com/{Username}";
         
         return await Task.FromResult<IActionResult>(Page());
     }
