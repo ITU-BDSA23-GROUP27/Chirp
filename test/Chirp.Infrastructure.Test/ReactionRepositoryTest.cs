fd9a9ad0e6dd43e26b90cea921a5ba8ffd9256d9
PTS404 <ptso@itu.dk>
2023-12-07 14:17:46 +0100

    Added GetLikeCount_ReturnsLikeCount() test

--- test/Chirp.Infrastructure.Test/ReactionRepositoryTest.cs
+++ test/Chirp.Infrastructure.Test/ReactionRepositoryTest.cs
@@ -42,38 +42,44 @@ public class ReactionRepositoryTest
         var cheep = new CheepDto { Message = "Hello World", UserName = user.Name, TimeStamp = DateTime.Now.ToString() };
         await _cheepRepository.CreateCheep(cheep);
         
+        var foundCheep = await _context.Cheeps.SingleAsync(c => c.Text == cheep.Message && c.User.Name == cheep.UserName);
+        var foundUser = await _context.Users.SingleAsync(u => u.Name == user.Name);
+        
         // Act
-        await _reactionRepository.LikeCheep(cheep.Id, user.Name);
+        await _reactionRepository.LikeCheep(foundCheep.CheepId, user.Name);
         
         // Assert
         var like = await _context.Reactions.SingleOrDefaultAsync(r => 
-            r.UserId == user.Id && 
-            r.CheepId == cheep.Id &&
+            r.UserId == foundUser.Id && 
+            r.CheepId == foundCheep.CheepId &&
             r.ReactionType == ReactionType.Like);
         
         Assert.NotNull(like);
-        Assert.Equal(user.Id, like.UserId);
-        Assert.Equal(cheep.Id, like.CheepId);
+        Assert.Equal(foundUser.Id, like.UserId);
+        Assert.Equal(foundCheep.CheepId, like.CheepId);
     }
 
     [Fact]
     public async Task LikeCheep_SuccessOnUnlike()
     {
         // Arrange
-        var user = new UserDto { Name = "Leo", Email = "leo@gmail.com"};
+        var user = new UserDto { Name = "Oleg", Email = "oleg@gmail.com"};
         await _userRepository.CreateUser(user);
     
         var cheep = new CheepDto { Message = "Hello World", UserName = user.Name, TimeStamp = DateTime.Now.ToString() };
         await _cheepRepository.CreateCheep(cheep);
         
+        var foundCheep = await _context.Cheeps.SingleAsync(c => c.Text == cheep.Message && c.User.Name == cheep.UserName);
+        var foundUser = await _context.Users.SingleAsync(u => u.Name == user.Name);
+        
         // Act
-        await _reactionRepository.LikeCheep(cheep.Id, user.Name); // Like
-        await _reactionRepository.LikeCheep(cheep.Id, user.Name); // Then unlike
+        await _reactionRepository.LikeCheep(foundCheep.CheepId, user.Name); // Like
+        await _reactionRepository.LikeCheep(foundCheep.CheepId, user.Name); // Then unlike
         
         // Assert
         var like = await _context.Reactions.SingleOrDefaultAsync(r => 
-            r.UserId == user.Id && 
-            r.CheepId == cheep.Id &&
+            r.UserId == foundUser.Id && 
+            r.CheepId == foundCheep.CheepId &&
             r.ReactionType == ReactionType.Like);
         
         Assert.Null(like);
@@ -81,6 +87,58 @@ public class ReactionRepositoryTest
 
     [Fact]
     public async Task GetLikeCount_ReturnsLikeCount()
+    {
+        // Arrange - Create users and cheep
+        var user1 = new UserDto { Name = "Uno", Email = "uno@gmail.com"};
+        await _userRepository.CreateUser(user1);
+        
+        var user2 = new UserDto { Name = "Dos", Email = "dos@gmail.com"};
+        await _userRepository.CreateUser(user2);
+    
+        var cheep = new CheepDto { Message = "Hello World", UserName = user1.Name, TimeStamp = DateTime.Now.ToString() };
+        await _cheepRepository.CreateCheep(cheep);
+        
+        // Act - Add likes
+        var foundCheep = await _context.Cheeps.SingleAsync(c => c.Text == cheep.Message && c.User.Name == cheep.UserName);
+        
+        await _reactionRepository.LikeCheep(foundCheep.CheepId, user1.Name); // Like
+        await _reactionRepository.LikeCheep(foundCheep.CheepId, user2.Name); // Then unlike
+        
+        // Assert
+        var likeCount = await _reactionRepository.GetLikeCount(foundCheep.CheepId);
+        Assert.Equal(2, likeCount);
+    }
+    
+    [Fact]
+    public async Task GetLikesFromCheep_ReturnsLikes()
+    {
+        // Arrange
+        var user1 = new UserDto { Name = "One", Email = "one@gmail.com"};
+        await _userRepository.CreateUser(user1);
+        
+        var user2 = new UserDto { Name = "Two", Email = "two@gmail.com"};
+        await _userRepository.CreateUser(user2);
+    
+        var cheep = new CheepDto { Message = "Hello World", UserName = user1.Name, TimeStamp = DateTime.Now.ToString() };
+        await _cheepRepository.CreateCheep(cheep);
+        
+        var foundCheep = await _context.Cheeps.SingleAsync(c => c.Text == cheep.Message && c.User.Name == cheep.UserName);
+        var foundUser1 = await _context.Users.SingleAsync(u => u.Name == user1.Name);
+        var foundUser2 = await _context.Users.SingleAsync(u => u.Name == user2.Name);
+        
+        // Act
+        await _reactionRepository.LikeCheep(foundCheep.CheepId, user1.Name);
+        await _reactionRepository.LikeCheep(foundCheep.CheepId, user2.Name);
+        
+        // Assert
+        var likes = (await _reactionRepository.GetLikesFromCheep(foundCheep.CheepId)).ToList();
+        Assert.Equal(2, likes.Count());
+        Assert.Contains(likes, l => l.UserId == foundUser1.Id);
+        Assert.Contains(likes, l => l.UserId == foundUser2.Id);
+    }
+    
+    [Fact]
+    public async Task HasUserLiked_ReturnsTrueIfUserHasLiked()
     {
         
     }
