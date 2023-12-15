a58227d0ddd9433fc5fd71bef155d9018f601b52
Kasper Kirkegaard <kkni@itu.dk>
2023-12-15 20:50:35 +0100

    Added unit tests to Privacy.cshtml.cs

--- src/Chirp.Web/Pages/Privacy.cshtml.cs
+++ src/Chirp.Web/Pages/Privacy.cshtml.cs
@@ -6,13 +6,15 @@ namespace Chirp.Web.Pages;
 
 public class PrivacyModel : BasePageModel
 {
+    public ILogger<PrivacyModel> Logger => _logger;
+
     private readonly ILogger<PrivacyModel> _logger;
 
     public PrivacyModel(ILogger<PrivacyModel> logger)
     {
         _logger = logger;
     }
-    
+
     public async Task<IActionResult> OnPostAuthenticateLogin()
     {
         return await HandleAuthenticateLogin();
