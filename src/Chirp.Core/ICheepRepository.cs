d27cbc3aa4c2477bf5b919f72a5e6a75b7d2a53e
Tien Cam Ly <tily@itu.dk>
2023-12-17 12:33:10 +0100

    Added documentation for the interfaces

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -2,6 +2,10 @@ using Chirp.Core.DTOs;
 
 namespace Chirp.Core;
 
+/// <summary>
+/// Interface of the repository for handling Cheeps in the Chirp application.
+/// </summary>
+
 public interface ICheepRepository
 {
     public Task<IEnumerable<CheepDto>> GetCheeps();
--- src/Chirp.Core/IFollowerRepository.cs
+++ src/Chirp.Core/IFollowerRepository.cs
@@ -2,6 +2,12 @@ using Chirp.Core.DTOs;
 
 namespace Chirp.Core;
 
+/// <summary>
+/// Interface of the repository for handling both followers and followees in the Chirp application.
+/// Followers are the ones that follows the User
+/// Followees are the ones the User follows
+/// </summary>
+/// 
 public interface IFollowerRepository
 {
     public Task<IEnumerable<UserDto>> GetFollowersFromUser(string userName);
